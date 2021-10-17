eeff5cb4f3fb7d6196a2cd279d5d134cb46709d8
Rupali Mathe <rupalimathe18@gmail.com>
2021-10-17 17:47:32 +0530

    Code checkin for first test Scenerio 1) When string= empty return addition as 0 2) When String has only one number then return that number
    Signed-off-by: Rupali Mathe <rupalimathe18@gmail.com>

--- string-calculator-starter-master/src/main/java/calculator/StringCalculator.java
+++ string-calculator-starter-master/src/main/java/calculator/StringCalculator.java
@@ -2,8 +2,17 @@ package calculator;
 
 class StringCalculator {
 
-    public int add(String input) {
-        return 0;
-    }
+	public int add(String input) {
+		if (input.isEmpty()) {
+			return 0;
+		} else {
+			int addition = 0;
+			String[] arr = input.split(","); //Split the string using Comma and store it in Array
 
+			for (int i = 0; i < arr.length; i++) {
+				addition += Integer.parseInt(arr[i]);	//Perform the addition for each number present in Array 
+			}
+			return addition;
+		}
+	}
 }
